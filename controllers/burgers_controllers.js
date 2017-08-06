30271a03457ce4cda6638fd7cacc1aa91889fc62
Brandon Fuller <fulleb33@gmail.com>
2017-08-06 13:55:29 -0500

    burger

---
+++ controllers/burgers_controllers.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+var burger = require("../models/burger.js");
+
+router.get('/', function(req, res) {
+	burger.all(function(data) {
+		var hbsObject = {
+			burgers: data
+		};
+		console.log(hbsObject);
+		res.render('index', hbsObject);
+	});
+});
+
+router.post('/', function(req, res) {
+	burger.create([
+		"burger_name", "devoured"
+	], [
+		req.body.burger_name, req.body.devoured
+	], function() {
+		res.redirect('/');
+	});
+});
+
+module.exports = router;
